039f31780e438974ddde95367138019cf149190c
shyunku <whdudgns7321@gmail.com>
2020-07-08 23:01:19 +0900

    create frame of left-sidebar, main body

--- src/server.js
+++ src/server.js
@@ -2,8 +2,23 @@ const express = require('express');
 const app = express();
 const ejs = require('ejs');
 const path = require('path');
+const livereload = require("livereload");
+const connectLivereload = require("connect-livereload");
 
 const PORT = 8080;
+
+// open livereload high port and start to watch public directory for changes
+const liveReloadServer = livereload.createServer();
+liveReloadServer.watch(path.join(__dirname, 'public'));
+
+// ping browser on Express boot, once browser has reconnected and handshaken
+liveReloadServer.server.once("connection", () => {
+  setTimeout(() => {
+    liveReloadServer.refresh("/");
+  }, 80);
+});
+
+app.use(connectLivereload());
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './views'));
@@ -13,7 +28,7 @@ app.use(express.urlencoded({extended: true}));
 //app.use(favicon(path.join(__dirname, 'static', 'img', 'favicon.ico')));
 
 app.get('/', (req, res) => {
-    res.render('example');
+    res.render('template/index');
 });
 
 app.listen(PORT, () => {
