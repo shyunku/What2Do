bc16d9bb4e834d5ff5f354505bbeeab18f983d4b
shyunku <whdudgns7321@gmail.com>
2020-07-12 06:26:40 +0900

    0.1.0v update

--- src/static/js/home.js
+++ src/static/js/home.js
@@ -1,9 +1,40 @@
 $(()=>{
+    const addNewTodoItemInput = $('#new_todo_item_input');
+
+    // add new todo items
+    addNewTodoItemInput.on("keydown", function(e){
+        if(e.keyCode === 13){
+            const itemTitle = $(this).val();
+            $(this).val(``);
+
+            // add item named {title}
+            addTodoItem(itemTitle);
+        }
+    });
+
+
     debug();
 });
 
+function addTodoItem(title){
+    // Temporary
+    let preference = parseInt(Math.random()*100);
+    let colorQuery = getTodoItemBoxColorQuery(preference);
+
+    $('.todo-list').prepend(`
+    <div class="todo-item" style="background-color: ${colorQuery}">
+        <span class="item-title">${title}</span>
+        <div class="item-property-wrapper">
+            <span class="item-deadline">2019년 3월 14일</span>
+        </div>
+    </div>
+    `);
+
+    // Implement to upload item to server (later)
+}
+
 function debug(){
-    for(let i=0;i<35;i++){
+    for(let i=0;i<20;i++){
         let preference = parseInt(Math.random()*100);
         let colorQuery = getTodoItemBoxColorQuery(preference);
 
@@ -11,7 +42,7 @@ function debug(){
         <div class="todo-item" style="background-color: ${colorQuery}">
             <span class="item-title">todo item &nbsp;${i}</span>
             <div class="item-property-wrapper">
-                <span class="item-deadline">2019년 3월 14일</span>
+                <span class="item-deadline">~2019/3/14</span>
             </div>
         </div>
         `);
