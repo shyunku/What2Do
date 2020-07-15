cd671c890a9243b2f92b52573f9f19b2d867c4f4
shyunku <whdudgns7321@gmail.com>
2020-07-15 22:46:21 +0900

    0.1.5v update

--- src/static/js/home.js
+++ src/static/js/home.js
@@ -45,6 +45,20 @@ $(()=>{
         }
     });
 
+    $.datepicker.setDefaults({
+        prevText: '이전 달',
+        nextText: '다음 달',
+        closeText: '닫기',
+        currentText: '오늘',
+        monthNames: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+        monthNamesShort: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+        dayNames: ['일', '월', '화', '수', '목', '금', '토'],
+        dayNamesShort: ['일', '월', '화', '수', '목', '금', '토'],
+        dayNamesMin: ['일', '월', '화', '수', '목', '금', '토'],
+        showMonthAfterYear: true,
+        yearSuffix: '년',
+    });
+
     animate();
 });
 
@@ -94,8 +108,19 @@ function animate(){
     });
 
     const deadlinePicker = $('#deadline_picker_btn');
-    deadlinePicker.on("click", ()=>{
-        $('#deadline_picker').datepicker("show");
+
+    $('#deadline_picker').datepicker({
+        dateFormat: '기한: yy/mm/dd',
+        onSelect: function(text){
+            $('#deadline_picker_btn').addClass('selected');
+        }
+    });
+
+    $('#startline_picker').datepicker({
+        dateFormat: '시작일: yy/mm/dd',
+        onSelect: function(text){
+            $('#startline_picker_btn').addClass('selected');
+        }
     });
 }
 
